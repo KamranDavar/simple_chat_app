ec6d1f6adb12ba9a0025dc94eb7a98520e1e098d
Kamran Davar <kamran.davar.kd@gmail.com>
2022-12-29 12:02:48 +0330

    webserver created

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+import http from "http";
+import path from "path";
+import express from "express";
+import { Server } from "socket.io";
+
+const app = express();
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, "/frontend/build")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/frontend/build/index.html"));
+});
+const httpServer = http.Server(app);
+
+
+const PORT = process.env.PORT || 4000;
+httpServer.listen(PORT, () => {
+  console.log(`Server started at http://localhost:${PORT}`);
+});
